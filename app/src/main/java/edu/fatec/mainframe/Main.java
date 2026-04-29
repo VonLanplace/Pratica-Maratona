9050a907bd8fd0a64cc7197e95445c09e6c7f5a0
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-04-29 15:21:21 -0300

    feature: Main menu

--- app/src/main/java/edu/fatec/mainframe/Main.java
+++ app/src/main/java/edu/fatec/mainframe/Main.java
@@ -5,9 +5,62 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        boolean loop = true;
+        int opc;
 
-        TheMayans theMayans = new TheMayans(scn);
+        while (loop) {
+            for (int i = 0; i < 100; i++) System.out.println();
+            printMainMenu();
+            try {
+                opc = Integer.parseInt(scn.nextLine().trim());
+            } catch (NumberFormatException e) {
+                opc = 10;
+            }
 
-        theMayans.test();
+            switch (opc) {
+                case 1: {
+                    TheMayans theMayans = new TheMayans();
+                    theMayans.test();
+                }
+                break;
+                case 2: {
+
+                }
+                break;
+                case 3: {
+                    DecisãoDoAgricultor decisãoDoAgricultor = new DecisãoDoAgricultor();
+                    decisãoDoAgricultor.test();
+                }
+                break;
+                case 4: {
+                    AHeliconEAlem aHeliconEAlem = new AHeliconEAlem();
+                    aHeliconEAlem.test();
+                }
+                break;
+                case 9:
+                    System.out.println("Saindo");
+                    loop = false;
+                    break;
+                default:
+                    System.out.println("Opcao Inválida");
+            }
+            System.out.print("Continuar (ENTER): ");
+            scn.nextLine();
+        }
+    }
+
+    public static void printMainMenu() {
+        System.out.println(
+                """
+                        |-------------------------------|
+                        |             MENU              |
+                        |-------------------------------|
+                        | 1 - The Mayans                |
+                        | 2 - Scoreboard Classificatória|
+                        | 3 - Decisão do Agricultor     |
+                        | 4 - À Helicon e além          |
+                        | 9 - Sair                      |
+                        |-------------------------------|"""
+        );
     }
 }
