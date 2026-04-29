bdbd8a3c14a5d25c88ce4e579b0f12b970ada67a
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-04-29 17:20:28 -0300

    feature: ScoreboardClassificatoria

--- app/src/main/java/edu/fatec/mainframe/Main.java
+++ app/src/main/java/edu/fatec/mainframe/Main.java
@@ -63,5 +63,6 @@ public class Main {
                         | 9 - Sair                      |
                         |-------------------------------|"""
         );
+        System.out.print("Option: ");
     }
 }
