0731db56af3f47a92d31d590cc528d71a0c78f2c
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-07 17:06:19 -0300

    start BaseInimiga

---
+++ app/src/main/java/edu/fatec/mainframe/BaseInimiga.java
@@ -0,0 +1,36 @@
+package edu.fatec.mainframe;
+
+import java.util.Scanner;
+
+public class BaseInimiga {
+
+
+    public void run(Scanner scn){
+
+        int Ds;
+        int Ys;
+        {
+            String[] entrada = scn.nextLine().trim().split("\\s+");
+            Ds = Integer.parseInt(entrada[0]);
+            Ys = Integer.parseInt(entrada[1]);
+        }
+
+        int Dm;
+        int Ym;
+        {
+            String[] entrada = scn.nextLine().trim().split("\\s+");
+            Dm = Integer.parseInt(entrada[0]);
+            Ym = Integer.parseInt(entrada[1]);
+        }
+
+        int resp = 0;
+
+        System.out.println(resp);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BaseInimiga baseInimiga = new BaseInimiga();
+        baseInimiga.run(scanner);
+    }
+}
